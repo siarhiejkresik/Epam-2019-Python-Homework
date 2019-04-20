5f0a71f3d74550295f3cad17a2be4fce22ffc1bf
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2019-04-20 20:47:46 +0300

    Update calculator (parser class)

--- final_task/pycalc/calculator.py
+++ final_task/pycalc/calculator.py
@@ -3,22 +3,28 @@
 # import Tokens from pycalc.scaner
 
 
-def advance(token_class=None):
-    global token
-    if token_class and not token.is_instance(token_class):
-        raise Exception("Syntax error. Expected: " + token_class.__name__)
-    token = next()
-    return
+# def advance(token_class=None):
+#     global token
+#     if token_class and not token.is_instance(token_class):
+#         raise Exception("Syntax error. Expected: " + token_class.__name__)
+#     token = next()
+#     return
 
 
 class Symbol(object):
     lbp = 0
+
+    def __init__(self, parser):
+        self.parser = parser
 
     def prefix(self):
         raise NotImplementedError()
 
     def infix(self):
         raise NotImplementedError()
+
+    def expression(self, *args):
+        return self.parser.expression(*args)
 
     def is_instance(self, token_class):
         return isinstance(self, token_class)
@@ -28,7 +34,8 @@ class Symbol(object):
 
 
 class Number(Symbol):
-    def __init__(self, value):
+    def __init__(self, parser, value):
+        super().__init__(parser)
         self.value = int(value)
 
     def prefix(self):
@@ -39,11 +46,11 @@ class Add(Symbol):
     lbp = 110
 
     def prefix(self):
-        right = expression(130)
+        right = self.expression(130)
         return right
 
     def infix(self, left):
-        right = expression(self.lbp)
+        right = self.expression(self.lbp)
         return left + right
 
 
@@ -51,11 +58,11 @@ class Sub(Symbol):
     lbp = 110
 
     def prefix(self):
-        right = expression(130)
+        right = self.expression(130)
         return -right
 
     def infix(self, left):
-        right = expression(self.lbp)
+        right = self.expression(self.lbp)
         return left - right
 
 
@@ -63,60 +70,60 @@ class Mul(Symbol):
     lbp = 120
 
     def infix(self, left):
-        return left * expression(self.lbp)
+        return left * self.expression(self.lbp)
 
 
 class Pow(Symbol):
     lbp = 140
 
     def infix(self, left):
-        return left ** expression(self.lbp - 1)
+        return left ** self.expression(self.lbp - 1)
 
 
 class Eq(Symbol):
     lbp = 60
 
     def infix(self, left):
-        return left == expression(self.lbp)
+        return left == self.expression(self.lbp)
 
 
 class LeftParen(Symbol):
-    lbp = 170
+    # lbp = 170
 
     def prefix(self):
-        expr = expression()
-        advance(RightParen)
+        expr = self.expression()
+        self.parser.advance(RightParen)
         return expr
 
 
 class RightParen(Symbol):
 
     def infix(self, left):
-        return expression(self.lbp)
+        return self.expression(self.lbp)
 
 
 class Function(Symbol):
     lbp = 160
 
     def prefix(self):
-        advance(LeftParen)
-        print('in fn:', token)
+        self.parser.advance(LeftParen)
+        print('in fn:', self.parser.token)
         args = []
-        if not token.is_instance(RightParen):
+        if not self.parser.next_token.is_instance(RightParen):
             while True:
-                args.append(expression(0))
-                if not isinstance(token, Comma):
+                args.append(self.expression())
+                if not self.parser.next_token.is_instance(Comma):
                     break
-                advance(Comma)
+                self.parser.advance(Comma)
         print('arguments:', args)
 
-        advance(RightParen)
+        self.parser.advance(RightParen)
         return sum(args)
 
 
 class Comma(Symbol):
     def prefix(self):
-        expr = expression(self.lbp)
+        expr = self.expression(self.lbp)
         return expr
 
 
@@ -124,61 +131,68 @@ class End(Symbol):
     pass
 
 
-def tokenize(program):
-    tokens = program.split()
-    print(tokens)
+class Parser:
+    def __init__(self, source):
+        self.source = source
+        self.tokens = tokenize(source, self)
+
+        self.token = None
+        self.next_token = None
+
+    def parse(self):
+        self.next()
+        return self.expression()
+
+    def next(self):
+        self.token = self.next_token
+        self.next_token = next(self.tokens)
+
+    def advance(self, token_class=None):
+        if token_class and not self.next_token.is_instance(token_class):
+            raise Exception("Syntax error. Expected: " + token_class.__name__)
+        self.next_token = next(self.tokens)
+
+    def expression(self, rbp=0):
+        self.next()
+        left = self.token.prefix()
+        print('in expression start:', self.token, self.next_token, left)
+        while rbp < self.next_token.lbp:
+            self.next()
+            # print('t    :', t)
+            # print('token:', token)
+            # print('rbp  :', rbp)
+            left = self.token.infix(left)
+
+        return left
+
+
+def tokenize(source, parser):
+    tokens = source.split()
     for literal in tokens:
         print('literal:',  literal)
         if literal.isdigit():
-            yield Number(literal)
+            yield Number(parser, literal)
         elif literal == "+":
-            yield Add()
+            yield Add(parser)
         elif literal == "-":
-            yield Sub()
+            yield Sub(parser)
         elif literal == "*":
-            yield Mul()
+            yield Mul(parser)
         elif literal == "**":
-            yield Pow()
+            yield Pow(parser)
         elif literal == "(":
-            yield LeftParen()
+            yield LeftParen(parser)
         elif literal == ")":
-            yield RightParen()
+            yield RightParen(parser)
         elif literal == "fn":
-            yield Function()
+            yield Function(parser)
         elif literal == ",":
-            yield Comma()
+            yield Comma(parser)
         elif literal == "==":
-            yield Eq()
+            yield Eq(parser)
         else:
             raise SyntaxError('unknown operator: %s', literal)
-    yield End()
-
-
-def expression(rbp=0):
-    global token
-    t = token
-    token = next()
-    left = t.prefix()
-    print('in expression start:', t, token, left)
-    while rbp < token.lbp:
-        t = token
-        token = next()
-        print('t    :', t)
-        print('token:', token)
-        print('rbp  :', rbp)
-        left = t.infix(left)
-
-    return left
-
-
-def parse(program):
-    # try:
-    global token, next
-    next = tokenize(program).next
-    token = next()
-    return expression()
-    # except Exception as e:
-    # print(e)
+    yield End(parser)
 
 
 def run(expression):
@@ -189,6 +203,7 @@ def run(expression):
 
 
 if __name__ == "__main__":
-    program = '1 == 1'
+    program = 'fn ( 1 , 2 , 4 )'
     # print(eval(program))
-    print(parse(program))
+    p = Parser(program)
+    print(p.parse())
